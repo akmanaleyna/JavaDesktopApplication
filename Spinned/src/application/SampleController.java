d18a727768b7df78df398f7fa8ca2f9a1726b5e1
akmanaleyna <93182473+akmanaleyna@users.noreply.github.com>
2023-05-10 15:33:29 +0300

    Add files via upload

--- Spinned/src/application/SampleController.java
+++ Spinned/src/application/SampleController.java
@@ -3,10 +3,13 @@ package application;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.Spinner;
+import javafx.scene.control.SpinnerValueFactory;
 
 public class SampleController implements Initializable{
 
@@ -19,8 +22,30 @@ public class SampleController implements Initializable{
 	
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
-		// TODO Auto-generated method stub
 		
+		//Spinner'ı tanımlıyoruz ve min&max değerlerini ayarlıyoruz
+		SpinnerValueFactory<Integer>  valueFactory = 
+				new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 10);
+		valueFactory.setValue(1);
+		
+		mySpinner.setValueFactory(valueFactory);
+		
+		currentValue = mySpinner.getValue();
+		
+		myLabel.setText(Integer.toString(currentValue));
+		
+		mySpinner.valueProperty().addListener(new ChangeListener<Integer>() {
+
+			@Override
+			public void changed(ObservableValue<? extends Integer> arg0, Integer arg1, Integer arg2) {
+
+				currentValue = mySpinner.getValue();
+				
+				myLabel.setText(Integer.toString(currentValue));
+				
+			}
+		});
 	}
 	
 }
+
